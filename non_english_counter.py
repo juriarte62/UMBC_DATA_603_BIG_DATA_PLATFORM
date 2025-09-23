8d03f96bc48a4049bf20c1b079155d2dff67cd20
Jonathan Uriarte <jonathanuriartelopez@gmail.com>
2025-09-23 00:14:28 -0400

    non english word count added

---
+++ non_english_counter.py
@@ -0,0 +1,25 @@
+from mrjob.job import MRJob
+import re
+from spellchecker import SpellChecker
+
+# spell = SpellChecker() # defauls
+spell = SpellChecker(language='en') # specify that we should use the English Dictionary
+
+WORD_RE = re.compile(r"[\w']+")
+
+class MRWordFreqCount(MRJob):
+
+    def mapper(self, _, line):
+        for word in WORD_RE.findall(line):
+            if word not in spell:
+                yield word.lower(), 1
+
+    def combiner(self, word, counts):
+        yield word, sum(counts)
+
+    def reducer(self, word, counts):
+        yield word, sum(counts)
+
+
+if __name__ == '__main__':
+    MRWordFreqCount.run()
